2be2013e558d62a60cf0df0bf444844a5ee9511f
Michael Emo <memo@iemosoft.com>
2020-09-02 13:34:58 -0500

    removed unneeded methods

--- public/linkedin/js/linkedInApp.js
+++ public/linkedin/js/linkedInApp.js
@@ -6,43 +6,6 @@
         console.log(data);
     }
 
-    const _candidatesMarshallToAlisonUIRequested = async () => {
-        //post message can only handle about 20 candidates at a time, break it up
-        tslog("Alision UI Marshalling Request");
-        const groupedCandidates = [];
-        const copy = [...candidates];
-        const groupBy = 15;
-
-        while (copy.length) {
-            groupedCandidates.push(copy.splice(0, groupBy));
-        }
-
-        for(var i=0; i<groupedCandidates.length; i++){
-            const isThisTheLastBatch = i === groupedCandidates.length-1;
-            callTonkaSource('candidatesMarshallToAlisonUIResponse', { candidates: groupedCandidates[i], isThisTheLastBatch});
-            await sleep(400);
-        }
-
-        tsLog("Marshalling complete");
-    }
-
-    const _getCandidatePublicProfile = async (candidateId) => {
-        //const found = candidates.find(c => c.id === candidateId);
-        const found = candidates[candidateId];
-        if (!found){
-            tsLog("Unable to find candidate " + candidateId);
-            return;
-        }
-
-        const linkedInRecruiterUrl = found.linkedInRecruiterUrl;
-        const recruiterProfile = httpGetJson(linkedInRecruterUrl);
-        await sleep(3000);
-    }
-
-    const _scrapeCandidateResults = async() => {
-        window.scrapedCandidates = await searchResultsScraper.scrapeResults();
-    }
-
     const _changeBadgeColor = (memberId, color) => {
         try {     
             const query = `#search-result-${memberId} abbr`;
@@ -60,9 +23,6 @@
 
     class LinkedInApp {
         sendMessageToCandidates = _sendMessageToCandidates;
-        candidatesMarshallToAlisonUIRequested = _candidatesMarshallToAlisonUIRequested;
-        getCandidatePublicProfile = _getCandidatePublicProfile;
-        scrapeCandidateResults = _scrapeCandidateResults;
         candidateUnselect = _candidateUnselect;
         changeBadgeColor = _changeBadgeColor;
         user = "";
