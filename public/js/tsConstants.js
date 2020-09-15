31bb056f019911bb86bec23199823ac86b68c1b3
Joe Harstad <joe.harstad@tonkasource.com>
2020-09-15 11:39:51 -0500

    FunctionsUrlForLocalhost: Fixing URL for localhost functions

--- public/js/tsConstants.js
+++ public/js/tsConstants.js
@@ -2,7 +2,7 @@
 
     class TSConstants {
         HOSTING_URL = "http://localhost:5001";
-        FUNCTIONS_URL = "http://localhost:5000";
+        FUNCTIONS_URL = "http://localhost:5000/alison-krause/us-central1";
     }
 
     window.tsConstants = new TSConstants();
