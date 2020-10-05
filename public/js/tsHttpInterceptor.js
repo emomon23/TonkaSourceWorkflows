e019afff5593e1734fcbe4eaed1f501a37448a82
Joe Harstad <joe.harstad@tonkasource.com>
2020-10-05 00:24:51 -0500

    contactInfoScraper:  Fix issue if responseText cannot be executed based on response type

--- public/js/tsHttpInterceptor.js
+++ public/js/tsHttpInterceptor.js
@@ -9,7 +9,11 @@
             var responseListeners = __responseIntercepts.filter(rl => rl.method === method.toLowerCase() && url.toLowerCase().indexOf(rl.urlContains) >= 0);
             responseListeners.forEach((rl) => {
                 if (rl.callBack) {
-                    rl.callBack({response: this.response, responseText: this.responseText, url: this.responseURL});
+                    let text = "";
+                    if (this.responseType === 'text' || this.responseType === '') {
+                        text = this.responseText;
+                    }
+                    rl.callBack({response: this.response, responseText: text, url: this.responseURL});
                 }
             });
         });
