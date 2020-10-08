329b992a0ae2ecf3a95f94fa899ad5cb7ed81e61
Michael Emo <memo@iemosoft.com>
2020-10-08 15:38:44 -0500

    scrap job experience from public profile

--- public/linkedin/js/scrapers/linkedInPublicProfileScraper.js
+++ public/linkedin/js/scrapers/linkedInPublicProfileScraper.js
@@ -75,21 +75,44 @@
         return result;
     }
 
-    const _scrapeNameAndLocation = (candidate) => {
-       /*fullName: 'ul[class*="pv-top-card"] li',
-        degreeConnection: 'ul[class*="pv-top-card"] span',
-        location: 'ul[class*="list-bullet"] li*/
-        const selectors = linkedInSelectors.publicProfilePage;
+    const _scrapeFirstAndLastNameFromProfile = () => {
+        let element = tsUICommon.findFirstDomElement(['button[aria-label*="Connect with"]', 'span[class*="a11y-text"]:contains("profile via message")', 'span[class*="a11y-text"]:contains("profile to PDF")', 'span[class*="a11y-text"]:contains("Report or block")']);
+        if (element === null){
+            return null;
+        }
 
-        //Alan Haggerty, MSEE
-        const fullName = $(selectors.fullName)[0].textContent.trim();
-        const degrees = $(selectors.degreeConnection)[0].textContent.trim();
+        let wholeName =  $(element).prop("tagName") === "BUTTON" ? $(element).attr('aria-label') : $(element).text();
+        ["Connect with ", "Share ", "Save ","’s profile via message", "' profile via message", "profile via message", "'s profile to PDF", "' profile to PDF", "profile to PDF", "Report or block"].forEach((remove) => {
+            wholeName = wholeName.split(remove).join('');
+        })
+
+        wholeName = wholeName.split(',')[0].trim(); //This will handle "Alan Haggerty, MCEE"
+        if (wholeName.indexOf('(') === 0){
+            wholeName = wholeName.substr(wholeName.indexOf(' ')); //This will handle "(75) Alan Haggerty"
+        }
+
+        const firstAndLast = wholeName.split(' ');
+        let result = {}
+
+        if (firstAndLast.length > 1){
+            result.firstName = firstAndLast[0];
+            firstAndLast.splice(0, 1);
+            result.lastName = firstAndLast.join(' ');
+        }
+
+        return result;
+    }
+
+    const _scrapeNameAndLocation = (candidate) => {
+        const selectors = linkedInSelectors.publicProfilePage;
+        const flName = _scrapeFirstAndLastNameFromProfile();
         
         //Minneapolis-St. Paul
         const location = $(selectors.location)[0].text.replace('Greater', '').replace('Area', '').trim();
 
-
-
+        candidate.firstName = flName.firstName;
+        candidate.lastName = flName.lastName;
+        candidate.cityHints = location.split('-')[0];
     }
 
     const _scrapeProfile = async () => {
@@ -97,15 +120,12 @@
             _scrapeNameAndLocation(scrapedCandidate);
             scrapedCandidate.positions = await _scrapeJobHistory();
 
-            const cachedCandidate = await _findCachedCandidate();
-            if (cachedCandidate) {
-                ca
-                searchResultsScraper.persistToLocalStorage();
+            const cachedCandidate = await _findCachedCandidate(scrapedCandidate);
+            if (cachedCandidate){
+                scrapedCandidate.memberId = cachedCandidate.memberId;
             }
             await linkedInApp.upsertContact(scrapedCandidate);
-
-            return updatedCandidate;
-       
+            return scrapedCandidate;
     }
 
     class LinkedInPublicProfileScraper {
