1f6fba9ea9e9228fca8e06a762f4086a63a697cf
Michael Emo <memo@iemosoft.com>
2020-11-24 13:29:53 -0600

    opp repository

--- public/linkedin/js/repositories/opportunitiesRepository.js
+++ public/linkedin/js/repositories/opportunitiesRepository.js
@@ -3,18 +3,13 @@
     const VERSION = 1;
     const SCHEMA = [
         {
-            storeName: 'note',
-            idProperty: 'noteId',
-            indexes: ['text']
-        },
-        {
-            storeName: 'connectionHistory',
-            idProperty: 'connectionId',
-            indexes: ['memberId', 'headline', 'imageUrl', 'lastName']
+            storeName: 'opportunity',
+            idProperty: 'id',
+            indexes: ['name', 'companyName', 'contactName']
         }
     ];
 
     const _storeFactory = baseIndexDbFactory.createStoreFactory(TONKA_SOURCE_DATABASE, VERSION, SCHEMA);
-    window.tsNoteRepo = _storeFactory.createStore('note');
-    window.tsConnectionHistoryRepo = _storeFactory.createStore('connectionHistory');
+    window.opportunityRepository = _storeFactory.createStore('opportunity');
+
 })();
